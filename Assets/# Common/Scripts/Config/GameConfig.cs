697f1566aa88f3851111d934049f0a308f324854
Andrey Yarov <yarov@alarstudios.com>
2022-05-16 14:20:35 +0400

    Create game settings and NPC template

---
+++ Assets/# Common/Scripts/Config/GameConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameConfig", menuName = "Test-Bombs/GameConfig")]
+public class GameConfig : ScriptableObject
+{
+    [SerializeField] private List<NpcTemplate> npcTemplates = new List<NpcTemplate>();
+    public int npcTemplateCount => npcTemplates.Count;
+    public NpcTemplate GetNpcTemplate(int index) => npcTemplates[index];
+}
---
+++ Assets/# Common/Scripts/Config/NpcTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class NpcTemplate
+{
+    [SerializeField, Tooltip("Prefab for each NPC spawned by this template")]
+    private NpcController prefab;
+
+    [SerializeField, Min(1), Tooltip("Health of each NPC spawned by this template")]
+    private int health;
+
+    [SerializeField, Min(0), Tooltip("Count of spawned NPCs\nZero is infinity")]
+    private int count;
+
+    [SerializeField, Min(0f), Tooltip("Delay between NPCs spawn in seconds\nNot 0 if count is infinity")]
+    private float spawnDelay;
+
+    [SerializeField, Min(0f), Tooltip("Speed of each NPC spawned by this template\nZero if stand still")]
+    private float speed;
+
+    public NpcController Prefab => prefab;
+    public int Health => health;
+    public int Count => count;
+    public float SpawnDelay => spawnDelay;
+    public float Speed => speed;
+    public bool ReadyForSpawn => prefab && (count > 0 || spawnDelay > 0f);
+}
