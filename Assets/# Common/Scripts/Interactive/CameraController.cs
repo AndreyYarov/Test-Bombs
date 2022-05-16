ab242b80a856f52b3f6ce539940a0a995a0ca627
Andrey Yarov <yarov@alarstudios.com>
2022-05-16 13:51:21 +0400

    Create controller for game camera

---
+++ Assets/# Common/Scripts/Interactive/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Vector3 pivot = Vector3.zero;
+    [SerializeField] private float rotationSensitivity = 3f;
+
+    private void Update()
+    {
+        float rot = Input.GetAxis("Mouse X");
+        if (rot != 0f)
+            transform.RotateAround(pivot, Vector3.up, rot * rotationSensitivity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(pivot, 0.25f);
+    }
+}
